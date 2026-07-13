647cd17c3bb72621e8f4135c4b9e6cf3f4633dfb
madcox2023 <145722120+madcox2023@users.noreply.github.com>
2026-07-13 23:40:56 +0100

    Initialize Firebase app and Firestore database

--- js/firebase.js
+++ js/firebase.js
@@ -1,1 +1,25 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.16.0/firebase-app.js";
 
+import {
+    getFirestore
+} from "https://www.gstatic.com/firebasejs/12.16.0/firebase-firestore.js";
+
+export const firebaseConfig = {
+
+  apiKey: "AIzaSy...",
+
+  authDomain: "algeria-order-system.firebaseapp.com",
+
+  projectId: "algeria-order-system",
+
+  storageBucket: "algeria-order-system.firebasestorage.app",
+
+  messagingSenderId: "865814720381",
+
+  appId: "1:865814720381:web:c9e7bec77af884f5aae614"
+
+};
+
+const app = initializeApp(firebaseConfig);
+
+export const db = getFirestore(app);
