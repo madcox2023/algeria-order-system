2f5e2b8a361b2ebb1147e2f21ee68a92b215b304
madcox2023 <145722120+madcox2023@users.noreply.github.com>
2026-07-14 12:20:12 +0100

    Update dashboard.js

--- admin/dashboard.js
+++ admin/dashboard.js
@@ -1,9 +1,49 @@
-document
-.getElementById("loginForm")
-.addEventListener("submit",(e)=>{
+import { db } from "../js/firebase.js";
 
-    e.preventDefault();
+import {
+    collection,
+    query,
+    orderBy,
+    onSnapshot
+} from "https://www.gstatic.com/firebasejs/12.16.0/firebase-firestore.js";
 
-    alert("سنربط تسجيل الدخول بـ Firebase في الخطوة التالية.");
+const ordersContainer = document.getElementById("orders");
+
+const q = query(
+    collection(db, "orders"),
+    orderBy("createdAt", "desc")
+);
+
+onSnapshot(q, (snapshot) => {
+
+    ordersContainer.innerHTML = "";
+
+    snapshot.forEach((doc) => {
+
+        const order = doc.data();
+
+        ordersContainer.innerHTML += `
+
+<div class="order-card">
+
+<h3>${order.name}</h3>
+
+<p>📞 ${order.phone}</p>
+
+<p>📍 ${order.wilaya} - ${order.commune}</p>
+
+<p>🚚 ${order.shippingType}</p>
+
+<p>📦 الكمية : ${order.quantity}</p>
+
+<p>💰 ${order.total} دج</p>
+
+<p>الحالة : ${order.status}</p>
+
+</div>
+
+`;
+
+    });
 
 });
