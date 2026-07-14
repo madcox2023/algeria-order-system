628a336dbcd093790ef7d6b1812cf103572f1b85
madcox2023 <145722120+madcox2023@users.noreply.github.com>
2026-07-14 01:16:47 +0100

    Update login.js

--- admin/login.js
+++ admin/login.js
@@ -1,1 +1,39 @@
+import { auth } from "../js/firebase.js";
 
+import {
+    signInWithEmailAndPassword
+} from "https://www.gstatic.com/firebasejs/12.16.0/firebase-auth.js";
+
+const form = document.getElementById("loginForm");
+
+form.addEventListener("submit", async (e) => {
+
+    e.preventDefault();
+
+    const email =
+        document.getElementById("email").value.trim();
+
+    const password =
+        document.getElementById("password").value;
+
+    try {
+
+        await signInWithEmailAndPassword(
+            auth,
+            email,
+            password
+        );
+
+        window.location.href = "dashboard.html";
+
+    }
+
+    catch(error){
+
+        alert("البريد الإلكتروني أو كلمة المرور غير صحيحة");
+
+        console.error(error);
+
+    }
+
+});
